1c6b1375c8f55709394b54316ed8a0f09726f738
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2019-10-03 22:18:49 -0300

    notas para el viernes

--- SegundoParcialUTNFRGP20192c/main.cpp
+++ SegundoParcialUTNFRGP20192c/main.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
-///probando git
+/**probando git
+
+
+ notas/tareas:
+ preguntar por que los gets usan en caso de ser char la variable en modo de puntero por ejemplo
+ de: intro a poo pagina 10
+
+    preguntar si una clase es un TDA (tipo de dato abstracto)
+
+ char * Articulo::getDescripcion(){
+ return descripcion;
+ }
+
+ (se usa el tipo de dato despues la funcion y con los dos puntos
+    se especifica a que metodo de la clase se desea llamar )
+
+    los gets suelen ser return de la variable de la clase
+    (propiedad que se desea leer.)
+
+    en caso de querer setear un string de caracteres
+    se tiene que usar la funcion strcpy desde una varuable
+    que se envia a el metodo mediante puntero de char.
+
+    los metodos son las funciones que posee cada objeto. ()
+    las propiedades son las variables que posee cada objeto.
+
+    (generalmente son de tipo private
+        o protected segun el caso por cuestiones de encapsulamiento.)
+
+**/
+
 
 int main()
 {
