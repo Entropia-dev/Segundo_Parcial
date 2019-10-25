37bbaaf9e517d57f7de054e96dd764f5962e0534
Entropia-dev <54455913+Entropia-dev@users.noreply.github.com>
2019-10-25 11:51:55 -0300

    cambios_desde_lab

--- SegundoParcialUTNFRGP20192c/main.cpp
+++ SegundoParcialUTNFRGP20192c/main.cpp
@@ -10,17 +10,26 @@ using namespace std;
 #include "Menues.h"
 
 
-/**probando git
+/**
+
+    POR HACER EN EL TP:
+    -EMPROLIJAR LOS MENUES PRINCIPALES. (TAMBIEN DEJARLOS FUNCIONALES ALMENOS SIN FUNCIONES)
+    -REALIZAR LOS METODOS DE GUARDAR DE TODAS LAS CLASES.
+    -REALIZAR LOS METODOS DE CARGAR DE TODAS LAS CLASES.
+    -REALIZAR LOS METODOS DE MOSTRAR DE TODAS LAS CLASES.
+
+      || PARA EL PROTOTIPO SE NECESITA EL ABML ALMENOS SIN NINGUNA VALIDACION  ||
+
 
 	el trabajo terminado es la nota del segundo parcial
 
-	paso 1 definir grupo y tema
+	paso 1 definir grupo y tema -----------------------------------------TERMINADO
 
-	paso 2 presentacion del informe (puede estar desaprobado)
+	Paso 2 presentacion del informe (puede estar desaprobado)----------------------TERMINADO
 	hay dos tipos 1 para un sistema de gestion y otro para un juego en el campus
 
 	para la entrega del prototipo se necesita el informe aprobado
-	cuando traemos el prototipo tenemos que traer el informe.
+	cuando traemos el prototipo tenemos que traer el informe.   ------------- EL LUNES 1/11 SE NECESITA EL PROTOTIPO
 
 	si llega el 22 de noviemre y no tenes el prototipo aprobado ,
 
@@ -28,7 +37,7 @@ using namespace std;
 
 	en las mesas de final se puede venir con el prototipo o con el programa terminado
 
-	se viene a las 8 am y despues de que los compañeros rindan se ve lo que trajiste para corregir.
+	se viene a las 8 am y despues de que los compaÃ±eros rindan se ve lo que trajiste para corregir.
 
 	recuperatorios del primero va a haber uno en noviempre y uno en febrero.
 
@@ -42,42 +51,7 @@ using namespace std;
 
 
 
-	===========================================================
-
-
-
-
-
-    base pdf:intro_a_POO
-
- notas/tareas:
- preguntar por que los gets usan en caso de ser char la variable en modo de puntero por ejemplo
- de: intro a poo pagina 10
-
-  char * Articulo::getDescripcion(){
- return descripcion;
- }
-
-    preguntar si una clase es un TDA (tipo de dato abstracto)
-
-
-
- (se usa el tipo de dato despues la funcion y con los dos puntos
-    se especifica a que metodo de la clase se desea llamar )
-
-    los gets suelen ser return de la variable de la clase
-    (propiedad que se desea leer.)
-
-    en caso de querer setear un string de caracteres
-    se tiene que usar la funcion strcpy desde una varuable
-    que se envia a el metodo mediante puntero de char.
-
-    los metodos son las funciones que posee cada objeto. ()
-    las propiedades son las variables que posee cada objeto.
-
-    (generalmente son de tipo private
-        o protected segun el caso por cuestiones de encapsulamiento.)
-
+	====================================================
 
       //////////////////////////////////  ejemplo de clase: /////////////////////////////////////////////////
 
@@ -93,72 +67,44 @@ using namespace std;
         };
 
                 void Articulo::Cargar(){
-        cout<<”INGRESE EL CODIGO: “;
+        cout<<â€INGRESE EL CODIGO: â€œ;
         cin>>codArt;
-        cout<<”INGRESE LA DESCRIPCION: ”;
+        cout<<â€INGRESE LA DESCRIPCION: â€;
         cin>>descripcion;
-        cout<<”INGRESE EL PRECIO UNITARIO: “;
+        cout<<â€INGRESE EL PRECIO UNITARIO: â€œ;
         cin>>pu;
-        cout<<”INGRESE EL STOCK”;
+        cout<<â€INGRESE EL STOCKâ€;
         cin>>stock;
         }
         void Articulo::Mostrar(){
-        cout<<” CODIGO: “;
+        cout<<â€ CODIGO: â€œ;
         cout<<codArt<<endl;
-        cout<<” DESCRIPCION: ”;
+        cout<<â€ DESCRIPCION: â€;
         cout<<descripcion<<endl;
-        cout<<” PRECIO UNITARIO: “;
+        cout<<â€ PRECIO UNITARIO: â€œ;
         cout<<pu<<endl;
-        cout<<” STOCK”;
+        cout<<â€ STOCKâ€;
         cout<<stock<<endl;
         }
-
-           //////////////////////////////////  Sets y Gets  /////////////////////////////////////////////////
-
-            char * Articulo::getCodArt(){
-            return codArticulo; }
-
-
-            char * Articulo::getDescripcion(){
-            return descripcion; }
-
-            float Articulo::getPu(){
-            return pu;  }
-
-            int Articulo::getStock(){return stock; }
-
-
-            void Articulo::setCodArt(char *cod){
-            strcpy(codArticulo, cod); }
-
-            void Articulo::setDescripcion(char *desc){
-            strcpy(descripcion, desc); }
-
-            void Articulo::setPu(float p){
-            pu=p;   }
-
-            void Articulo::setStock(int s){
-            stock=s; }
-
-
         ===========================================================
-
-
-
-
-
 **/
 
 
 int main()
 {
-    persona obj;
-    char nombre_persona[50];
-    cout<<"ingrese nombre persona"<<endl;
-    cin>>nombre_persona;
-    obj.set_nombre(nombre_persona);
-    cout<<"se mostrara el nombre de la persona"<<endl;
-    cout<<"NOMBRE PERSONA:"<<obj.get_nombre()<<endl;
-
+    int anio = 1000;
+    int mes = 1 ;
+    int dia = 1;
+    venta obj;
+    Fecha Nueva_fecha;
+   /// se carga un objeto de tipo fecha para enviarlo y que sea el objeto de la clase venta
+    Nueva_fecha.setAnio(anio);
+    Nueva_fecha.setDia(dia);
+    Nueva_fecha.setMes(mes);
+    /// seteo la fecha de venta mediante el objeto previamente cargado
+    obj.setFecha_venta(Nueva_fecha);
+      ///obtengo el dia de la fecha de venta
+        cout<<obj.getFecha().getDia();
     return 0;
 }
+
