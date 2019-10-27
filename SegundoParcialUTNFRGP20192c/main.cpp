e0ae9b059a7456def67398b837299165e04f2b4c
claudiovass31876292 <54720366+claudiovass31876292@users.noreply.github.com>
2019-10-26 22:23:22 -0300

    23332
    agregacion de menu

--- SegundoParcialUTNFRGP20192c/main.cpp
+++ SegundoParcialUTNFRGP20192c/main.cpp
@@ -18,19 +18,19 @@ using namespace std;
     -REALIZAR LOS METODOS DE CARGAR DE TODAS LAS CLASES.
     -REALIZAR LOS METODOS DE MOSTRAR DE TODAS LAS CLASES.
 
-      || PARA EL PROTOTIPO SE NECESITA EL ABML ALMENOS SIN NINGUNA VALIDACION  ||
+      || PARA EL PROTOTIPO SE NECESITA EL ABML AL MENOS SIN NINGUNA VALIDACION  ||
 
 
 	el trabajo terminado es la nota del segundo parcial
 
 	paso 1 definir grupo y tema -----------------------------------------TERMINADO
 
-	Paso 2 presentacion del informe (puede estar desaprobado)----------------------TERMINADO
+	Paso 2 presentacion del informe (INFORME APROVADO)----------------------TERMINADO
 	hay dos tipos 1 para un sistema de gestion y otro para un juego en el campus
 
-	para la entrega del prototipo se necesita el informe aprobado
 	cuando traemos el prototipo tenemos que traer el informe.   ------------- EL LUNES 1/11 SE NECESITA EL PROTOTIPO
 
+	para la entrega del prototipo se necesita el informe aprobado
 	si llega el 22 de noviemre y no tenes el prototipo aprobado ,
 
 	o el programa terminado no se consigue y se terminaron estan las mesas de final
@@ -95,16 +95,89 @@ int main()
     int anio = 1000;
     int mes = 1 ;
     int dia = 1;
-    venta obj;
+    cliente cli;
+    venta v;
     Fecha Nueva_fecha;
    /// se carga un objeto de tipo fecha para enviarlo y que sea el objeto de la clase venta
     Nueva_fecha.setAnio(anio);
     Nueva_fecha.setDia(dia);
     Nueva_fecha.setMes(mes);
     /// seteo la fecha de venta mediante el objeto previamente cargado
-    obj.setFecha_venta(Nueva_fecha);
+    v.setFecha_venta(Nueva_fecha);
       ///obtengo el dia de la fecha de venta
-        cout<<obj.getFecha().getDia();
+        cout<<v.getFecha().getDia();
+       char opc[20],opcventas[20],opcclientes[20];
+     int opswitch,OPCVENTAS,OPCCLIENTES;
+     int pos=0,NUMVMODIF;
+     bool salir=false;
+
+        MenuPrincipal();
+        cout<<"INGRESE UNA OPCION: ";
+        cin.getline(opc,20);
+     opswitch=strToIntAll(opc);
+     while(!salir)
+     {
+         switch(opswitch)
+         {
+           case 1:{ menuVentas();
+           cout<<"INGRESE UNA OPCION: ";
+           cin.getline(opcventas,20);
+           OPCVENTAS=strToIntAll(opcventas);
+           switch(OPCVENTAS)
+           {
+              case 1: {
+                 // v.cargar();
+                  cin.get();
+                      }break;
+              case 2: {
+             // v.mostrar();
+              cin.get();
+                      }break;
+              case 3: {
+                  cout<<"Ingrese el numero de la venta a modificar: ";
+                  cin>>NUMVMODIF;
+              //actualizarSaldo(NUMVMODIF);
+              }break;
+              case 4:{
+              //ModificarVenta();
+              }break;
+        default: cout<<"OPCION INCORRECTA";
+        //Sleep(1000);
+           }//cierra case 1
+         case 2:{
+           menuClientes();
+           cout<<"INGRESE UNA OPCION: ";
+           cin.getline(opcclientes,20);
+           OPCCLIENTES=strToIntAll(opcclientes);
+           switch(OPCCLIENTES)
+           {
+             case 1: {
+             //cli.cargar();
+             //cli.mostrar();
+            cin.get();
+            }break;
+
+           }
+         case 3:
+            {
+               // menuModelos();
+                cin.get();
+            }break;
+         case 0:{
+         salir=true;
+         exit(1);
+         }break;
+        default: cout<<"OPCION INCORRECTA";
+        //Sleep(1000);
+        }
+        }//cierra switch principal
+     MenuPrincipal();
+     cout<<"INGRESE UNA OPCION: ";
+     cin.getline(opc,20);
+     }//cierra while
+     }
+
     return 0;
+//cierra main
 }
 
